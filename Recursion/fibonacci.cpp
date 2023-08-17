04bbdf2d9d2771ff814cc90495a4ad2086c3a507
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-08-18 00:58:35 +0530

    Solved

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -3,21 +3,18 @@ using namespace std;
 
 int fibonacci(int n)
 {
-    if (n == 0)
+    if (n == 0 || n == 1)
     {
-        return 0;
+        return n;
     }
-    else if (n == 1)
-    {
-        return 1;
-    }
+
     else
         return fibonacci(n - 1) + fibonacci(n - 2);
 }
 int main()
 {
 
-    cout << fibonacci(4);
+    cout << fibonacci(5);
 
     return 0;
 }
