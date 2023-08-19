d2077792d096440adfeaff287c3504236c4acc2f
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-08-19 18:14:26 +0530

    Solved

---
+++ Recursion/tilingProblem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int tiles(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return tiles(n - 1) + tiles(n - 2);
+}
+int main()
+{
+    cout << tiles(4);
+    return 0;
+}
