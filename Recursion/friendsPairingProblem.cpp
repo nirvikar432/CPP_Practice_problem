36c151bd790d9c820d88b6c468914c3c77644c75
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-08-19 18:40:41 +0530

    Difficult app

--- Recursion/friendsPairingProblem.cpp
+++ Recursion/friendsPairingProblem.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int friendsPair(int n)
+{
+    if (n == 0 || n == 1 || n == 2)
+    {
+        return n;
+    }
+
+    return friendsPair(n - 1) + friendsPair(n - 2) * (n - 1);
+}
 int main()
 {
+
+    cout << friendsPair(4);
 
     return 0;
 }
