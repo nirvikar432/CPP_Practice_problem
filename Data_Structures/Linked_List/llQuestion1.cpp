5e2bf762a8ccd07567bcde59b507fff0bdb503bf
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-08-22 01:27:48 +0530

    Solved

--- Data_Structures/Linked_List/llQuestion1.cpp
+++ Data_Structures/Linked_List/llQuestion1.cpp
@@ -1,8 +1,80 @@
 #include <iostream>
 using namespace std;
 
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+    LinkedList()
+    {
+        head = NULL;
+    }
+
+    void insertAtEnd(int val)
+    {
+        Node *new_node = new Node(val);
+        if (head == NULL)
+        {
+            head = new_node;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+void deleteAlt(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL && temp->next != NULL)
+    {
+        Node *currentPos = temp->next;
+        temp->next = temp->next->next;
+        free(currentPos);
+        temp = temp->next;
+    }
+}
 int main()
 {
+    LinkedList ll;
+    ll.insertAtEnd(1);
+    ll.insertAtEnd(2);
+    ll.insertAtEnd(3);
+    ll.insertAtEnd(4);
+    ll.insertAtEnd(5);
+    ll.insertAtEnd(6);
+
+    ll.display();
+    deleteAlt(ll.head);
+    ll.display();
 
     return 0;
 }
