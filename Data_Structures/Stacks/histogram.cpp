6c53eef368fbce723fb45fb34b331ce12ad8a0ba
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-09-02 22:14:14 +0530

    Solved

---
+++ Data_Structures/Stacks/histogram.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int histogram(vector<int> &arr)
+{
+    int size = arr.size();
+    int ans = INT_MIN;
+    stack<int> st;
+    st.push(0);
+    for (int i = 1; i < size; i++)
+    {
+        while (!st.empty() && arr[i] < arr[st.top()])
+        {
+            int element = arr[st.top()];
+            int nse = i;
+            st.pop();
+            int pse = (st.empty()) ? (-1) : st.top();
+            ans = max(ans, element * (nse - pse - 1));
+        }
+        st.push(i);
+    }
+
+    while (!st.empty())
+    {
+        int element = arr[st.top()];
+        int nse = size;
+        st.pop();
+        int pse = (st.empty()) ? (-1) : st.top();
+        ans = max(ans, element * (nse - pse - 1));
+    }
+
+    return ans;
+}
+int main()
+{
+    int n;
+    cout << "Enter vector Size : ";
+    cin >> n;
+    vector<int> input;
+    while (n--)
+    {
+        int inp;
+        cin >> inp;
+        input.push_back(inp);
+    }
+
+    int ans = histogram(input);
+    cout<<ans<<endl;
+
+    return 0;
+}
+
+// 13
+// 2 1 5 6 2 3 0 2 1 5 6 2 3
