61810d02585fbbab264ae473e80e08db5f7e60e8
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-09-03 23:21:49 +0530

    Solved

---
+++ Data_Structures/Stacks/prefixEvaluation.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<stack>
+#include<math.h>
+using namespace std;
+
+int calculate(int n1,int n2, char op){
+    if(op == '+'){
+        return n1 + n2;
+    }
+    if(op == '-'){
+        return n1 - n2;
+    }
+    if(op == '/'){
+        return n1 / n2;
+    }
+    if(op == '^'){
+        return pow(n1, n2);
+    }
+    return n1* n2;
+}
+
+int eval(string &str){
+    stack<int> st;
+    for(int i= str.size()-1;i>=0;i--){
+        char ch = str[i];
+        if(isdigit(ch)){
+            st.push(ch - '0');
+        }
+        else{
+            int n2 = st.top();
+            st.pop();
+            int n1 = st.top();
+            st.pop();
+
+            st.push(calculate(n1,n2,ch));
+        }
+    }
+    return st.top();
+}
+int main()
+{
+    string str ="-9+*531";
+    cout<<eval(str);
+    
+    return 0;
+}
