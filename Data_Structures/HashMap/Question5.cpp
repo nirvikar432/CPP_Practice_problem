d262c474b261a2a9392a9c61ee08c24be1474f9f
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-09-30 00:44:13 +0530

    Solved

--- Data_Structures/HashMap/Question5.cpp
+++ Data_Structures/HashMap/Question5.cpp
@@ -1,30 +1,44 @@
 // Longest sub array equal to 0
-
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 using namespace std;
-
-vector<int> longestSubArray(vector<int> v)
+int longestSubArray(vector<int> &v)
 {
     unordered_map<int, int> map;
+    int prefixSum = 0;
+    int maxLen = 0;
+
+    for(int i=0;i<v.size();i++){
+        prefixSum+=v[i];
+        if(prefixSum ==0){
+            maxLen++;
+        }
+        if(map.find(prefixSum)!=map.end()){
+            maxLen = max(maxLen, i-map[prefixSum]);
+
+        }
+        else{
+            map[prefixSum] = i;
+        }
+    }
+
+    return maxLen;
 }
 int main()
 {
     int n;
+    cin>>n;
     vector<int> v(n);
-    for (int i = 0; i < v.size(); i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> v[i];
     }
-
-    vector<int> ans(2);
-
-    ans = longestSubArray(v);
-    for (int i = 0; i < 2; i++)
-    {
-        cout << ans[i] << " ";
-    }
-
+    cout<<longestSubArray(v);
     return 0;
 }
+
+
+// 8
+// 15 -2 2 -8 1 7 10 23
+// 5
