a8c3577d03620e40b0cf05d6281e2f140590c498
NIRVIKAR <nirvikarprasoon@gmail.com>
2024-09-12 01:33:33 +0530

    Problem Solved

--- Data_Structures/Stacks/compilerBalanced.cpp
+++ Data_Structures/Stacks/compilerBalanced.cpp
@@ -2,12 +2,14 @@
 #include <stack>
 using namespace std;
 
-int bracketBalance(string s)
+void bracketBalance(string s)
 {
     stack<char> op;
     stack<int> nums;
 
     int size = s.length();
+    int oper = 0;
+    int bracket = 0;
     for (int i = 0; i < size; i++)
     {
         if (s[i] == '(')
@@ -18,11 +20,22 @@ int bracketBalance(string s)
         {
             nums.push(s[i] - '0');
         }
+        else if ((s[i] == '+' || s[i] == '*' || s[i] == '/' || s[i] == '-') && (s[i + 1] == ')'))
+        {
+            oper++;
+        }
+        else if (!op.empty() && s[i] == ')')
+        {
+            op.pop();
+        }
     }
+    cout << "Bracket :" << op.size() << "\nOperand :" << oper;
 }
 
 int main()
 {
+    string str = "(1+(2*(3-1+2+))+2";
+    bracketBalance(str);
 
     return 0;
 }
